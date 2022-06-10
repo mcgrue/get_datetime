3d1bb360e5d82d0395ac0649f5aaf6ac323f36ce
Ben McGraw <ben.mcgraw@gmail.com>
2022-06-10 00:34:03 -0700

    Crash Override?

--- action.py
+++ action.py
@@ -4,7 +4,15 @@ from pytz import timezone
 
 print('time zone is {0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os.environ['INPUT_TZ2']))
 
-fmt = "%Y%m%d_%H%M%S_%Z%z"
+external_fmt = os.environ['INPUT_DATEFORMAT_OVERRIDE']
+fmt = "%Y-%m-%d_%H.%M.%S_%Z%z"
+
+if external_fmt:
+    print('External format provided via environment variable INPUT_DATEFORMAT_OVERRIDE: {0:s} '.format(external_fmt))
+    fmt = external_fmt
+else:
+    print('Using default format: {0:s}'.format(fmt))
+
 now_utc = datetime.now(timezone('{0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os.environ['INPUT_TZ2'])))
 str_temp = now_utc.strftime(fmt)
 
