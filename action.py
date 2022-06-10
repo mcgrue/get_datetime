6387adcafac8ef21e8e12c5cecf215620034f16f
Ben McGraw <ben.mcgraw@gmail.com>
2022-06-10 01:06:57 -0700

    Tidy up some

--- action.py
+++ action.py
@@ -4,17 +4,10 @@ from pytz import timezone
 
 tz1 = os.environ.get('INPUT_TZ1', 'Etc')
 tz2 = os.environ.get('INPUT_TZ2', 'Greenwich')
-external_fmt = os.environ.get('INPUT_DATEFORMAT_OVERRIDE', '')
-
-fmt = '%Y-%m-%d_%H.%M.%S_%Z%z'
+fmt = os.environ.get('INPUT_DATEFORMAT_OVERRIDE', '%Y-%m-%d_%H.%M.%S_%Z%z')
 
 print('time zone is {0:s}/{1:s}'.format(tz1, tz2))
-
-if external_fmt:
-    print('External format provided via environment variable INPUT_DATEFORMAT_OVERRIDE: {0:s} '.format(external_fmt))
-    fmt = external_fmt
-else:
-    print('Using default format: {0:s}'.format(fmt))
+print('Using datetime format: {0:s}'.format(fmt))
 
 now_utc = datetime.now(timezone('{0:s}/{1:s}'.format(tz1, tz2)))
 str_temp = now_utc.strftime(fmt)
